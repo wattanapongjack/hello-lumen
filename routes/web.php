06cee2fadddda59b3b6bb38102acc6249c5dc488
wattanapongjack <wattanapong@carro.co>
2018-09-27 17:00:56 +0700

    Update a Book

--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,4 @@ $router->get('/books/{id:[\d]+}', [
     'uses' => 'BooksController@show'
 ]);
 $router->post('/books', 'BooksController@store');
+$router->put('/books/{id:[\d]+}', 'BooksController@update');
